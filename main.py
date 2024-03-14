786b772ee024cb20434f8c503d54a216b462fe70
NikolfaxW <Denisnik@cvut.cz>
2024-03-14 12:10:19 +0100

    The task is ready

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import numpy as np
+import itertools
+
 
 def print_hi(name):
     print(f'Hi, {name}')
+
+def factroial(n):
+    result = 1
+    for i in  range(1,n + 1):
+        result *= i
+    return result
+
 
 def get_submatrix(A, i_of_line, j_of_column):#works
     result = np.delete(A,i_of_line, 0)
@@ -21,7 +30,33 @@ def count_determinant_by_cofactor_method(A):
         return det
 
 def count_determinant_by_permutation_definition(A):
-    print("here")
+    n = A.shape[1]
+    permutations = generate_permutations(n)
+    result = 0;
+    number_of_perm = factroial(n)
+    for i in range(number_of_perm):
+        temp = pow(-1, count_inversions(permutations[i]))
+        for j in range(n):
+            temp *= A[j,permutations[i][j] - 1]
+        result +=temp
+    return result
+
+
+
+def generate_permutations(n):
+    nums = list(range(1, n + 1))
+    permutations = list(itertools.permutations(nums))
+    return permutations
+
+def count_inversions(perm):
+    inversions = 0
+    n = len(perm)
+    for i in range(n):
+        for j in range(i+1, n):
+            if perm[i] > perm[j]:
+                inversions += 1
+    return inversions
+
 
 if __name__ == '__main__':
     A = np.array([
@@ -29,11 +64,22 @@ if __name__ == '__main__':
         [1., -1., -1., 1.],
         [4., 2., 2., 1.],
         [8., 1., 1., 2.]])
+
     result_np = np.linalg.det(A)
     print("By np.linalg.det(A) function we got:", result_np) #det A = 27
+
     result_cof = count_determinant_by_cofactor_method(A)
     print("By cofactor method we got:", result_cof)
     if  np.allclose(result_cof, result_np):
         print("The result of count_determinant_by_cofactor_method(A) function \033[92mIS CLOSE\033[0m to np.linalg.det(A)")
     else:
         print("The result of count_determinant_by_cofactor_method(A) function \033[91mIS NOT CLOSE\033[0m to np.linalg.det(A)")
+
+    result_perm = count_determinant_by_permutation_definition(A)
+    print("By the permutation definitions we got:",result_perm)
+    if np.allclose(result_perm, result_np):
+        print(
+            "The result of count_determinant_by_permutation_definition(A) function \033[92mIS CLOSE\033[0m to np.linalg.det(A)")
+    else:
+        print(
+            "The result of count_determinant_by_permutation_definition(A) function \033[91mIS NOT CLOSE\033[0m to np.linalg.det(A)")
